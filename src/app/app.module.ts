c152eb64e84e650c17b9eeca4071dbc9184dc00c
Juliana Guzmán <juliguzmac1906@gmail.com>
2024-02-05 19:45:22 -0500

    Se crea de nuevo el proyecto con los componentes, modulos y servicios ha utilizar momentaneamente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { OrderComponent } from './components/order/order.component';
+import { WaiterComponent } from './components/waiter/waiter.component';
+import { KitchenComponent } from './components/kitchen/kitchen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    OrderComponent,
+    WaiterComponent,
+    KitchenComponent
   ],
   imports: [
     BrowserModule,
