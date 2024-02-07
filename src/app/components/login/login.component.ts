3ad5277849539bf2028f02a2d66b8ca94643e8a4
Juliana Guzmán <juliguzmac1906@gmail.com>
2024-02-07 09:04:25 -0500

    se crea el servicio de la autenticación de chat, se maneja la respuesta pero aún hay errores de compatabilidad

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { AuthResponse } from 'src/app/models/auth-model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  errorMessage: string = '';
+
+  constructor(private authService: AuthService, private formBuilder: FormBuilder) { 
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  credentialValidatorForm(): void {
+    if (this.loginForm.invalid) {
+      this.errorMessage = '*Credentials are invalid!';
+      return;
+    }
+    const { email, password } = this.loginForm.value;
+    this.authService.login(email, password).subscribe({
+      next: (response: AuthResponse) => {
+        localStorage.setItem('accessToken', response.accessToken);
+      },
+      error: (error) => {
+        if (error.status === 400) {
+          this.errorMessage = 'Debe proporcionar un correo electrónico y una contraseña.';
+        } else if (error.status === 404) {
+          this.errorMessage = 'Credenciales incorrectas. Verifique su correo electrónico y contraseña.';
+        } else {
+          this.errorMessage = 'Error desconocido. Por favor, inténtelo de nuevo.';
+        }
+      }
+    });
+
+  };
+  
+  get emailControl() {
+    return this.loginForm.get('email');
+  }
+
+  get passwordControl() {
+    return this.loginForm.get('password');
+  }
+
 }
+
+
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import { AuthResponse } from '../models/auth-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private apiBaseUrl = 'http://localhost:8080/'
+
+  constructor(private http: HttpClient) { }
+
+  login(email:string, password:string):Observable<AuthResponse> {
+    const body = {email,password};
+    return this.http.post<AuthResponse>(`${this.apiBaseUrl}/login`,body);
+  }
+
 }
